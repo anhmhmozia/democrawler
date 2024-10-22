1178fdc9d2badd544ab36bf78f0bf4da30f1e94e
AnhMH <anh.mh@moziasoft.com>
2024-10-22 18:27:47 +0700

    Init crawler

--- app.py
+++ app.py
@@ -1,4 +1,9 @@
 from flask import Flask, render_template, request, redirect, url_for, jsonify
+import asyncio
+import json
+from urllib.parse import urlparse, parse_qs
+from crawl4ai import AsyncWebCrawler
+from crawl4ai.extraction_strategy import JsonCssExtractionStrategy
 
 app = Flask(__name__)
 
@@ -8,10 +13,45 @@ def index():
     return render_template('index.html')
 
 @app.route('/submit_form', methods=['POST'])
-def submit_form():
-    data = request.json  # Get the JSON data from the AJAX request
-    # Process the data here (save to database, etc.)
-    return jsonify({"message": data}), 200
+async def submit_form():
+    data = request.json
+    crawlerData = []
+    crawlerUrl = data['url']
+    base_selector = data['base_selector']
+    fields = data['fields']
+    schema = {
+        "name": "Demo crawler",
+        "baseSelector": base_selector,
+        "fields": []
+    }
+    for field in fields:
+        if (field['type'] == 'text'):
+            schema['fields'].append(field)
+        elif (field['type'] == 'image'):
+            schema['fields'].append({
+                'name': field['name'],
+                'selector': field['selector'],
+                'type': 'attribute',
+                'attribute': 'src',
+            })
+        else:
+            schema['fields'].append({
+                'name': field['name'],
+                'selector': field['selector'],
+                'type': 'attribute',
+                'attribute': 'href',
+            })
+        extraction_strategy = JsonCssExtractionStrategy(schema, verbose=True)
+        async with AsyncWebCrawler(verbose=True) as crawler:
+            result = await crawler.arun(
+                url=crawlerUrl,
+                extraction_strategy=extraction_strategy,
+                bypass_cache=True
+            )
+
+            assert result.success, "Failed to crawl the page"
+            crawlerData = json.loads(result.extracted_content)
+    return jsonify({"message": crawlerData}), 200
     
 if __name__ == '__main__':
     app.run(debug=True)
